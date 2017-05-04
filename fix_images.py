21a4c380b35a24aee4115e7c69ea4e69cc47d0c5
Mike H <tapeworm09@hotmail.com>
2017-05-04 13:24:24 +0200

    image cropper

---
+++ fix_images.py
@@ -0,0 +1,87 @@
+import glob
+import cv2
+import os
+import sys
+
+def resize_crop(img, x):
+	h = img.shape[0];
+	w = img.shape[1];
+	
+	
+	
+	if(h<w):
+		ratio = float(w)/h;
+		new_h = x;
+		new_w = int(x * ratio);
+		crop_h0 = 0;
+		crop_h1 = x;
+		crop_w0 = int((new_w - x) / 2);
+		crop_w1 = crop_w0 + x;
+	else:
+		ratio = float(h)/w;
+		new_h = int(x * ratio);
+		new_w = x;
+		crop_w0 = 0;
+		crop_w1 = x;
+		crop_h0 = int((new_h - x) / 2);
+		crop_h1 = crop_h0 + x;
+		
+	img = cv2.resize(img,(new_w,new_h),interpolation = cv2.INTER_AREA);
+	img = img[crop_h0:crop_h1,crop_w0:crop_w1];
+	
+		
+	return img;
+
+# count = 500;
+# for filename in glob.iglob('data\Images\**\*.jpg', recursive=True):
+	# print(filename)
+	# count -= 1;
+	# if(count < 0):
+		# break;
+
+
+	
+	
+if(len(sys.argv) > 2):
+	cropsize = int(sys.argv[1]);
+	directory = sys.argv[2];
+else:
+	print("arguments: cropsize, output directory name")
+	sys.exit()
+	
+if not os.path.exists(directory):
+	os.makedirs(directory)
+	
+	
+all_images_fn = list(glob.iglob('data\Images\**\*.jpg', recursive=True));
+print(len(all_images_fn))
+
+
+for i,fn in enumerate(all_images_fn):
+	img = cv2.imread(fn)
+	img = resize_crop(img,cropsize);
+	cv2.imwrite(directory+"/"+str(i)+".png",img);
+	print("\r"+str(i)+"/"+str(len(all_images_fn)),end="\r")
+
+
+
+
+# cv2.imwrite('messigray.png',img)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
