188161569151a0867923435b9339a5b6d95e6534
unknown <>
2017-05-08 07:16:12 +0200

    Changed Adam parameters and batch size. Training can get stuck easily.

--- GAN_train.py
+++ GAN_train.py
@@ -34,7 +34,7 @@ def chunks(l, n):
     """Yield successive n-sized chunks from l."""
     for i in range(0, len(l), n):
         yield i,l[i:i+n]
-
+        # yield l[i:i+n]
 def train(paths, batch_size, EPOCHS):
 
     #reproducibility
@@ -45,16 +45,16 @@ def train(paths, batch_size, EPOCHS):
     IMAGES = load_shuffle(paths)
 
 
-    # BATCHES = [ b for b in chunks(IMAGES, batch_size) ]
+    BATCHES = [ b for b in chunks(IMAGES, batch_size) ]
 
     discriminator = GAN_models.discriminator()
     generator = GAN_models.generator()
     discriminator_on_generator = GAN_models.generator_containing_discriminator(generator, discriminator)
     #try default params setting
-    adam_gen = Adam()
-    adam_dis = Adam()
-    # adam_gen=Adam(lr=0.00002, beta_1=0.0005, beta_2=0.999, epsilon=1e-08)
-    # adam_dis=Adam(lr=0.00002, beta_1=0.0005, beta_2=0.999, epsilon=1e-08)
+    # adam_gen = Adam()
+    # adam_dis = Adam()
+    adam_gen=Adam(lr=0.00002, beta_1=0.0005, beta_2=0.999, epsilon=1e-08)
+    adam_dis=Adam(lr=0.00002, beta_1=0.0005, beta_2=0.999, epsilon=1e-08)
     generator.compile(loss='binary_crossentropy', optimizer=adam_gen)
     discriminator_on_generator.compile(loss='binary_crossentropy', optimizer=adam_gen)
     discriminator.trainable = True
@@ -65,7 +65,7 @@ def train(paths, batch_size, EPOCHS):
 
     #margin = 0.25
     #equilibrium = 0.6931
-    inter_model_margin = 0.10
+    inter_model_margin = 0.25
 
     for epoch in range(EPOCHS):
         print("Epoch {}".format(epoch))
@@ -79,38 +79,41 @@ def train(paths, batch_size, EPOCHS):
             else:
                 pass
 
-        generated_images = None#
+        # generated_images = None#
         for index, image_batch in chunks(IMAGES,batch_size):
+        # for index, image_batch in enumerate(BATCHES):
             print("Epoch {} Batch {}".format(epoch,index))
 
             Noise_batch = np.array( [ generate_code() for _ in range(len(image_batch)) ] )
             generated_images = generator.predict(Noise_batch)
-            for i, img in enumerate(generated_images):
-                cv2.imwrite('results/{}.jpg'.format(i), np.uint8(255 * 0.5 * (img + 1.0)))
+            # for i, img in enumerate(generated_images):
+            #     cv2.imwrite('results/{}.jpg'.format(i), np.uint8(255 * 0.5 * (img + 1.0)))
 
             Xd = np.concatenate((image_batch, generated_images))
             yd = [1] * len(image_batch) + [0] * len(image_batch) # labels
 
-            print("Training first discriminator..")
+            # print("Training first discriminator..")
             d_loss = discriminator.train_on_batch(Xd, yd)
 
             Xg = Noise_batch
             yg = [1] * len(image_batch)
 
-            print("Training first generator..")
+            # print("Training first generator..")
             g_loss = discriminator_on_generator.train_on_batch(Xg, yg)
 
             print("Generator loss: {} Discriminator loss: {} Total: {}".format(g_loss, d_loss, g_loss + d_loss))
             if g_loss < d_loss and abs(d_loss - g_loss) > inter_model_margin:#generator is better
+                print("Updating discriminator..")
                 while abs(d_loss - g_loss) > inter_model_margin:
-                    print("Updating discriminator..")
+                    # print("Updating discriminator..")
                     d_loss = discriminator.train_on_batch(Xd, yd)
                     print("Generator loss: {} Discriminator loss: {}".format(g_loss,d_loss))
                     if d_loss < g_loss:
                         break
             elif d_loss < g_loss and abs(d_loss - g_loss) > inter_model_margin:#discriminator is better
+                print("Updating generator..")
                 while abs(d_loss - g_loss) > inter_model_margin:
-                    print("Updating generator..")
+                    # print("Updating generator..")
                     g_loss = discriminator_on_generator.train_on_batch(Xg, yg)
                     print("Generator loss: {} Discriminator loss: {}".format(g_loss, d_loss))
                     if g_loss < d_loss:
@@ -126,7 +129,7 @@ def train(paths, batch_size, EPOCHS):
                 discriminator.save_weights('discriminator_weights', True)
 
         plt.clf()
-        for i, img in enumerate(generated_images[:5]):
+        for i, img in enumerate(generated_images[:9]):
             i = i+1
             plt.subplot(3, 3, i)
             plt.imshow(img)
@@ -164,7 +167,7 @@ def get_args():
 
 if __name__ == "__main__":
     # load_image('data128/0.png')
-    train('data128/',batch_size=128,EPOCHS=2)
+    # train('data128/',batch_size=64,EPOCHS=20)
     generate(2)
 
     # args = get_args()
